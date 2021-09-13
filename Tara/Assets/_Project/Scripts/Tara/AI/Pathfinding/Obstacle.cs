e46260523965d0e530898a3a9cc8c2008dd9a6fd
Jasch0Jumper <ja.coding7@gmail.com>
2021-09-13 19:51:31 +0200

    removed old gizmos

--- Tara/Assets/_Project/Scripts/Tara/AI/Pathfinding/Obstacle.cs
+++ Tara/Assets/_Project/Scripts/Tara/AI/Pathfinding/Obstacle.cs
@@ -44,18 +44,14 @@ namespace Tara.Pathfinding
 
 		[Header("Gizmos")]
 		[SerializeField] private bool showPoints = default;
-		[SerializeField] private bool showAllPoints = default;
-		[SerializeField] private bool showBlockPointsPath = default;
 		
 		private void OnDrawGizmos()
 		{
 			if (showPoints) { DrawPoints(); }
-			if (showBlockPointsPath) { DrawBlockPointPath(); }
 		}
 		private void OnDrawGizmosSelected()
 		{
 			DrawPoints();
-			if (showAllPoints) { DrawAllPoints(); }
 		}
 
 		private void DrawPoints()
@@ -66,30 +62,6 @@ namespace Tara.Pathfinding
 			{
 				Gizmos.DrawWireSphere(point, 1f);
 			}
-		}
-		private void DrawAllPoints()
-		{
-			Debug.LogWarning("Gizmo not implemented.", this);
-
-			//Gizmos.color = new Color(1f, 0.921568632f, 0.0156862754f, 0.25f);   //yellow with 50% alpha
-
-			//foreach (var point in blockedArea.GetPointsInArea(GridManager.cellSize))
-			//{
-			//	Gizmos.DrawWireSphere(point, GridManager.cellSize / 2);
-			//}
-		}
-		private void DrawBlockPointPath()
-		{
-			Debug.LogWarning("Gizmo not implemented.", this);
-
-			//Gizmos.color = Color.red;
-
-			//List<Vector3> blockedPoints = blockedArea.GetPointsInArea(GridManager.cellSize);
-
-			//for (int i = 0; i < blockedPoints.Count - 1; i++)
-			//{
-			//	Gizmos.DrawLine(blockedPoints[i], blockedPoints[i + 1]);
-			//}
 		}
 #endif
 		#endregion
