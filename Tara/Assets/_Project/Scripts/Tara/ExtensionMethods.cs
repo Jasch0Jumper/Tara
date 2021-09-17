0c2481c5f66361974e06a445c510ca864407de7b
Jasch0Jumper <ja.coding7@gmail.com>
2021-09-17 09:53:03 +0200

    added AsVector2()

--- Tara/Assets/_Project/Scripts/Tara/ExtensionMethods.cs
+++ Tara/Assets/_Project/Scripts/Tara/ExtensionMethods.cs
@@ -4,6 +4,7 @@ namespace Tara
 {
     public static class ExtensionMethods
     {
-		public static Vector3 AsVector3(this Vector2 vector2) => new Vector3(vector2.x, vector2.y, 0f);
+		public static Vector3 AsVector3(this Vector2 vector) => new Vector3(vector.x, vector.y, 0f);
+		public static Vector2 AsVector2(this Vector3 vector) => new Vector2(vector.x, vector.y);
 	}
 }
